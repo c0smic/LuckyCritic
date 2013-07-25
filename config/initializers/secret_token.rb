75c6adc531fbf3a0480a2f22795692d88dd9b26c
Jack Ketcham <c0smic@luckycritic.jackketcham.com>
2013-07-25 21:22:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luckycritic::Application.config.secret_token = '088afbc18842615217a85791240aa0240a075972ce06b32fab449567c5d3a0d2e483f9acab035903d65ffb64f493f4655ab3ead254f136dea60ba6355f1e5687'
